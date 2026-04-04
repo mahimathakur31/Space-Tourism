a17df468da244c24bcb2111e2b399f36752acf3d
Your Name <mahimagautam58@gmail.com>
2026-04-04 13:48:18 +0530

    Navbar_toggle_updt

--- src/components/header/atoms/navbar-toggler/NavbarToggler.jsx
+++ src/components/header/atoms/navbar-toggler/NavbarToggler.jsx
@@ -1,3 +1,24 @@
+// import React from 'react';
+// // import Close from '../../../../assets/shared/icon-close.ico';
+// // import Hamburger from '../../../../assets/shared/icon-hamburger.ico';
+// import './NavbarToggler.scoped.scss';
+
+// const NavToggler = ({ isNavBarOpen, clickHandler }) => {
+//   return (
+//     <img
+//       className={`nav-toggler ${isNavBarOpen ? 'nav-toggler--close' : ''}`}
+//       onClick={clickHandler}
+//       // src={isNavBarOpen ? Close : Hamburger}
+//       // alt='Navigation bar toggler'
+//     />
+//   );
+// };
+
+// NavToggler.displayName = 'NavbarToggler';
+// export default React.memo(NavToggler);
+
+// New header from llm 
+
 import React from 'react';
 // import Close from '../../../../assets/shared/icon-close.ico';
 // import Hamburger from '../../../../assets/shared/icon-hamburger.ico';
@@ -8,8 +29,8 @@ const NavToggler = ({ isNavBarOpen, clickHandler }) => {
     <img
       className={`nav-toggler ${isNavBarOpen ? 'nav-toggler--close' : ''}`}
       onClick={clickHandler}
-      // src={isNavBarOpen ? Close : Hamburger}
-      // alt='Navigation bar toggler'
+      src="#" // temporary placeholder, update with your actual icons if needed
+      alt="Navigation toggler" // THIS fixes the build error
     />
   );
 };
